33ab991590cfb606f4a4b2a46557b598da945f06
Felipe Matthew Moreira Nascimento <102431464+FelipeMatthew@users.noreply.github.com>
2023-10-19 22:18:18 -0300

    Add files via upload

--- api/src/main/java/br/com/estudio/estudio/repository/IUserRepository.java
+++ api/src/main/java/br/com/estudio/estudio/repository/IUserRepository.java
@@ -3,7 +3,9 @@ package br.com.estudio.estudio.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.estudio.estudio.model.UserModel;
 
+@Repository
 public interface IUserRepository extends JpaRepository<UserModel, UUID> {}
